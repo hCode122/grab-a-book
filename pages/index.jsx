b772f5cba66d0f61520860909664609604b88334
hCode122 <hCode122@protonmail.me>
2023-09-18 15:06:09 +0300

    Basic skeleton

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,16 @@
-function Main() {
+import MyApp from "./_app";
+
+// pages/index.js
+export default function Home() {
     return (
-        <div>
-            <title>Grab A Book</title>
+      <div className="container-fluid p-3 vh-100">
+        <h1 className="text-light bg-dark w-25 margin-left p-1" >Grab A Book   |= =|</h1>
+        <div className="container-fluid h-25 border-bottom border-dark d-flex justify-content-center align-items-center
+        gap" style={{gap: 10 + 'px'}}>
+            <input className="w-25 h-25"></input> 
+            <button className="btn h-25 btn-dark" style={{width: 3 +'vw'}}>Go</button>
         </div>
-    )
-}
+        
+      </div>
+    );
+  }
