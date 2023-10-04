286e0cfacca221c8a2e96b5964c79c2100d030f6
hCode122 <hCode122@protonmail.me>
2023-10-04 10:55:59 +0300

    Added a workaround to be able to build NextJs app with non-page files inside pages directory

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    
+  }
+module.exports = {
+    pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+}
+   
+  module.exports = nextConfig
