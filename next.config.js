be23534c2fd5d5a99bff0e0ed53fe4cc98707902
hCode122 <hCode122@protonmail.me>
2023-10-04 10:49:48 +0300

    fixed a syntax error in next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    module.exports = {
-        pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
-    }
+    
   }
+module.exports = {
+    pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+}
    
   module.exports = nextConfig
